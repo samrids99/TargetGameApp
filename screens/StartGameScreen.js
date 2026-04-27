b49e10426a4b39647e13759b851c4f245946fe25
Sam Ridsdale <samridsdale108@gmail.com>
2026-04-27 19:02:19 +0100

    learning to use useWindowDimensions react hook to detect orientation change and adapt

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,10 @@
-import { Alert, TextInput, View, StyleSheet } from "react-native";
+import {
+  Alert,
+  TextInput,
+  View,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
 import { useState } from "react";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
@@ -8,6 +14,8 @@ import InstructionText from "../components/ui/InstructionText";
 
 function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
+
+  const { width, height } = useWindowDimensions();
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -30,8 +38,10 @@ function StartGameScreen({ onPickNumber }) {
     onPickNumber(chosenNumber);
   }
 
+  const marginTopDistance = height < 380 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title style={styles.titleText}>Guess My Number</Title>
       <Card>
         <InstructionText>Enter a Number</InstructionText>
@@ -62,7 +72,7 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: 100,
+    // marginTop: deviceHeight < 400 ? 30 : 100,
     alignItems: "center",
   },
   numberInput: {
